e262aaf1ba2e87538657b762086bc433e88b6877
Xiaohan Zhang <hellocharlien@hotmail.com>
2025-12-21 21:06:06 -0700

    basic player_controller

---
+++ player_controller.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+
+@export var move_speed: float = 200.0
+
+func _physics_process(delta):
+  var direction = Vector2.ZERO
+  
+  if Input.is_action_pressed("camera_right"):
+    direction.x += 1
+  if Input.is_action_pressed("camera_left"):
+    direction.x -= 1
+  if Input.is_action_pressed("camera_down"):
+    direction.y += 1
+  if Input.is_action_pressed("camera_up"):
+    direction.y -= 1
+  
+  velocity = direction.normalized() * move_speed
+  move_and_slide()
