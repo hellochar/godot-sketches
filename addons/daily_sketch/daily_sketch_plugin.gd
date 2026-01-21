3e123165cd5d81e380df3159a41baea70f632071
hellochar <hellocharlien@hotmail.com>
2026-01-21 12:17:09 -0800

    daily_sketch_plugin

---
+++ addons/daily_sketch/daily_sketch_plugin.gd
@@ -0,0 +1,66 @@
+@tool
+extends EditorPlugin
+
+func _enter_tree() -> void:
+  add_tool_menu_item("Create Daily Sketch", _create_daily_sketch)
+
+func _exit_tree() -> void:
+  remove_tool_menu_item("Create Daily Sketch")
+
+func _create_daily_sketch() -> void:
+  var date := Time.get_date_dict_from_system()
+  var month_names := ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"]
+  var month: String = month_names[date.month - 1]
+  var day := str(date.day)
+  var year := str(date.year)
+
+  var folder_name := "%s_%s_%s" % [month, day, year]
+  var folder_path := "res://%s" % folder_name
+  var script_name := "%s.gd" % folder_name
+  var scene_name := "%s.tscn" % folder_name
+
+  var dir := DirAccess.open("res://")
+  if dir.dir_exists(folder_name):
+    push_error("Folder already exists: %s" % folder_path)
+    return
+
+  dir.make_dir(folder_name)
+
+  var script_content := """extends Control
+
+
+func _ready() -> void:
+  pass
+
+
+func _process(delta: float) -> void:
+  pass
+"""
+
+  var script_file := FileAccess.open("%s/%s" % [folder_path, script_name], FileAccess.WRITE)
+  script_file.store_string(script_content)
+  script_file.close()
+
+  var script_uid := ResourceUID.id_to_text(ResourceUID.create_id())
+
+  var scene_content := """[gd_scene load_steps=2 format=3]
+
+[ext_resource type="Script" uid="%s" path="%s/%s" id="1_script"]
+
+[node name="Control" type="Control"]
+layout_mode = 3
+anchors_preset = 15
+anchor_right = 1.0
+anchor_bottom = 1.0
+grow_horizontal = 2
+grow_vertical = 2
+script = ExtResource("1_script")
+""" % [script_uid, folder_path, script_name]
+
+  var scene_file := FileAccess.open("%s/%s" % [folder_path, scene_name], FileAccess.WRITE)
+  scene_file.store_string(scene_content)
+  scene_file.close()
+
+  EditorInterface.get_resource_filesystem().scan()
+
+  print("Created daily sketch: %s" % folder_path)
