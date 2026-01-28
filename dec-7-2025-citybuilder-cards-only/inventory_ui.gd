8486740ec951bb8916080b97abb0b3a14d4e8f06
hellochar <hellocharlien@hotmail.com>
2026-01-27 23:23:07 -0800

    rename dec-7-2025 to citybuilder-cards-only
    So, it was basically an idle game. the structure
    production did occasionally make interesting things
    happen. Giving all resources a 'base' property was
    *kind of* good but also made everything revolve
    around peasant population. which... maybe isn't a bad
    idea. But some of the cards were just fucking *OP*.
    Like, +1 food. Or +1 grass and grass just solves the
    food problem perfectly.
    
    That being said, being able to get up to t2 and t3
    random resources when the rng was just right did feel
    kind of good.
    
    I'm not sure that random parameters of structures offers
    any benefit over just hand-authoring the e.g. 100 structures
    that are relevant and interesting (also allows for
    special art and narrative for each structure).
    
    Perhaps a followup experiment is to add PoE-like
    affixes.

---
+++ dec-7-2025-citybuilder-cards-only/inventory_ui.gd
@@ -0,0 +1,63 @@
+extends VBoxContainer
+class_name InventoryUI
+
+@onready var title_label: Label = %Title
+@onready var cards_container: FlowContainer = %CardsContainer
+
+const ITEM_CARD = preload("res://dec-7-2025-citybuilder-cards-only/item_card.tscn")
+
+var inventory: World.Inventory
+var needs_refresh: bool = false
+
+@export var accept_drops: bool = true
+@export var allow_drag: bool = true
+var drop_filter: Callable
+
+func set_inventory(inv: World.Inventory) -> void:
+  inventory = inv
+  inv.on_changed.connect(func() -> void:
+    needs_refresh = true
+  )
+  refresh()
+
+func _process(_delta: float) -> void:
+  if needs_refresh:
+    refresh()
+
+func refresh() -> void:
+  needs_refresh = false
+  if not cards_container:
+    return
+
+  for child in cards_container.get_children():
+    child.queue_free()
+
+  if not inventory:
+    return
+
+  if title_label:
+    inventory.compute_name()
+    title_label.text = inventory.name
+
+  for item in inventory.dict.keys():
+    var card = ITEM_CARD.instantiate()
+    cards_container.add_child(card)
+    card.set_item(item, inventory.dict[item])
+    card.source_inventory = inventory if allow_drag else null
+
+func _can_drop_data(_pos: Vector2, data: Variant) -> bool:
+  if not accept_drops or not inventory:
+    return false
+  if data is not Dictionary or not data.has("item"):
+    return false
+  if data.get("source") == inventory:
+    return false
+  if drop_filter.is_valid() and not drop_filter.call(data.get("item"), data.get("source")):
+    return false
+  return true
+
+func _drop_data(_pos: Vector2, data: Variant) -> void:
+  var item: Item = data.get("item")
+  var amount: int = data.get("amount")
+  var source: World.Inventory = data.get("source")
+  inventory.take_from(source, item, amount)
