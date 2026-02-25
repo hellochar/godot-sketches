ba030abc3d4b3a52f4479f7faf2da08147fc2cce
hellochar <hellocharlien@hotmail.com>
2026-02-24 16:51:45 -0800

    walker - R to add road, Z to delete

--- feb_20_2026-walker-spawner/feb_20_2026.gd
+++ feb_20_2026-walker-spawner/feb_20_2026.gd
@@ -1,7 +1,12 @@
 extends Node
 
-func _ready() -> void:
-  pass
+@onready var roads: TileMapLayer = %roads
 
-func _process(_delta: float) -> void:
-  pass
+func _unhandled_input(event: InputEvent) -> void:
+  if event is InputEventKey and event.pressed and not event.echo:
+    var mouse_pos := roads.get_local_mouse_position()
+    var cell := roads.local_to_map(mouse_pos)
+    if event.keycode == KEY_R:
+      roads.set_cells_terrain_connect([cell], 1, 0)
+    elif event.keycode == KEY_Z:
+      roads.set_cells_terrain_connect([cell], 1, -1)
